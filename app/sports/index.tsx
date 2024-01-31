208a30a2e5cb92937f9747c96d4383868b5edd7f
sparrow-js <octocat@github.com>
2024-01-31 12:48:51 +0800

    sports commit message

---
+++ app/sports/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Sports() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <p>Home</p>
+        </main>
+    );
+}
+      
+    
